ef336782e31cedcdfaf5a384ab4effcc2e3e9d69
Joyce Luiza <joyceluizabrito@gmail.com>
2020-11-03 19:14:25 -0300

    resources_managment_v1.1

--- server/db.js
+++ server/db.js
@@ -2,10 +2,11 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "pipoca",
+    password: "insira a sua senha aqui",
     host: "localhost",
-    port: 5432,
+    port: 5432, // verifique se esta é a porta configurada pelo postgres
     database: "resource_register"
 });
 
 module.exports = pool;
+
