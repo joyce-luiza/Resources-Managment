afbc78e399f23fc95eb10e8ed0bcf986ae472324
joyce-luiza <61090216+joyce-luiza@users.noreply.github.com>
2020-11-05 18:17:18 -0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "pipoca",
+    password: "insira sua senha aqui",
     host: "localhost",
     port: 5432, // verifique se esta é a porta configurada pelo postgres
     database: "resource_register"
